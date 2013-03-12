f70aa539cca3058bd11df7283574348f2d5b44af
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-12 10:48:27 -0400

    Deleted commented out LeapYear solution

--- Haskell/LeapYear/LeapYear.hs
+++ Haskell/LeapYear/LeapYear.hs
@@ -2,9 +2,4 @@ module LeapYear where
 
 isLeapYear :: Int -> Bool
 
---isLeapYear n 
---  | n `mod` 400 == 0                   = True
---  | n `mod` 4 == 0 && n `mod` 100 /= 0 = True
---  | otherwise                          = False
-
 isLeapYear n = (n `mod` 400 == 0) || (n `mod` 4 == 0 && n `mod` 100 /= 0)
