ca48b71090cd51a1266a757a4fe2a14346163b10
Michael Gilliland <mjg.py3@gmail.com>
2013-04-01 02:49:31 -0400

    Finished tennis scoring in C#

--- C#/TennisScoring/TennisScoring.cs
+++ C#/TennisScoring/TennisScoring.cs
@@ -1,16 +1,28 @@
+using System;
+
 class TennisScoring
 {
     private int _playerOne, _playerTwo;
 
     public TennisScoring(int score1=0, int score2=0)
     {
-        
+        SetScores(score1, score2);
     }
 
-    public void SetScores(scorePlayer1, scorePlayer2)
+    public void SetScores(int scorePlayer1, int scorePlayer2)
     {
         _playerOne = scorePlayer1;
         _playerTwo = scorePlayer2;
+    }
+
+    public string GetPlayer1ScoreName()
+    {
+        return GetScoreName(_playerOne);
+    }
+
+    public string GetPlayer2ScoreName()
+    {
+        return GetScoreName(_playerTwo);
     }
 
     public bool HasPlayer1Won()
@@ -26,5 +38,38 @@ class TennisScoring
     private bool DidFirstScoreWin(int score1, int score2)
     {
         return (score1 > score2) && (score1-score2 >= 2);
+    }
+
+    private string GetScoreName(int score)
+    {
+        if (_playerOne == _playerTwo && _playerOne >= 3)
+        {
+            return "deuce";
+        }
+        if (Math.Abs(_playerOne-_playerTwo) == 1 && (_playerOne+_playerTwo) >= 6)
+        {
+            if (_playerOne == score && _playerOne > _playerTwo)
+            {
+                return "advantage";
+            }
+            if (_playerTwo == score && _playerTwo > _playerOne)
+            {
+                return "advantage";
+            }
+        }
+
+        switch (score)
+        {
+            case 0:
+                return "love";
+            case 1:
+                return "fifteen";
+            case 2:
+                return "thirty";
+            case 3:
+                return "forty";
+            default:
+                return "";
+        }
     }
 }
--- C#/TennisScoring/TennisScoringTest.cs
+++ C#/TennisScoring/TennisScoringTest.cs
@@ -3,4 +3,117 @@ using NUnit.Framework;
 [TestFixture]
 class TennisScoringTest
 {
+    [Test]
+    public void AZeroZeroGameHasNoWinner()
+    {
+        TennisScoring game = new TennisScoring();
+
+        game.SetScores(0, 0);
+        Assert.IsFalse(game.HasPlayer1Won());
+        Assert.IsFalse(game.HasPlayer2Won());
+    }
+
+    [Test]
+    public void AFourToTwoGameIsAWin()
+    {
+        TennisScoring game = new TennisScoring();
+
+        game.SetScores(4, 2);
+        Assert.IsTrue(game.HasPlayer1Won());
+        Assert.IsFalse(game.HasPlayer2Won());
+
+        game.SetScores(2, 4);
+        Assert.IsTrue(game.HasPlayer2Won());
+        Assert.IsFalse(game.HasPlayer1Won());
+    }
+
+    [Test]
+    public void NoWinnerInAThreeToFourGame()
+    {
+        TennisScoring game = new TennisScoring();
+
+        game.SetScores(3, 4);
+        Assert.IsFalse(game.HasPlayer2Won());
+        Assert.IsFalse(game.HasPlayer1Won());
+    }
+
+    [Test]
+    public void AGameWithMoreThanFourIsNotWonUnlessMoreThanTwoAbove()
+    {
+        TennisScoring game = new TennisScoring();
+
+        game.SetScores(7, 6);
+        Assert.IsFalse(game.HasPlayer2Won());
+        Assert.IsFalse(game.HasPlayer1Won());
+
+        game.SetScores(7, 5);
+        Assert.IsTrue(game.HasPlayer1Won());
+        Assert.IsFalse(game.HasPlayer2Won());
+    }
+
+    [Test]
+    public void GetScoreNameWorksExpectedlyForPlayerOne()
+    {
+        TennisScoring game = new TennisScoring();
+        string [] expected = {"love",
+                              "fifteen",
+                              "thirty", 
+                              "forty",
+                              ""};
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            game.SetScores(i, 0);
+            Assert.AreEqual(expected[i], game.GetPlayer1ScoreName());
+        }
+    }
+
+    [Test]
+    public void GetScoreNameWorksExpectedlyForPlayerTwo()
+    {
+        TennisScoring game = new TennisScoring();
+        string [] expected = {"love",
+                                            "fifteen",
+                                            "thirty", 
+                                            "forty",
+                                            ""};
+
+        for (int i = 0; i < 5; i += 1)
+        {
+            game.SetScores(0, i);
+            Assert.AreEqual(expected[i], game.GetPlayer2ScoreName());
+        }
+    }
+
+    [Test]
+    public void DueceWorksExpectedlyForSomeSimpleCases()
+    {
+        TennisScoring game = new TennisScoring();
+
+        game.SetScores(3, 3);
+
+        Assert.AreEqual("deuce", game.GetPlayer1ScoreName());
+        Assert.AreEqual("deuce", game.GetPlayer2ScoreName());
+
+        game.SetScores(7, 7);
+
+        Assert.AreEqual("deuce", game.GetPlayer1ScoreName());
+        Assert.AreEqual("deuce", game.GetPlayer2ScoreName());
+    }
+
+    [Test]
+    public void AdvantageWorksExpectedllyForSomeSimpleCases()
+    {
+        TennisScoring game = new TennisScoring();
+
+        game.SetScores(3, 4);
+
+        Assert.AreEqual("advantage", game.GetPlayer2ScoreName());
+        Assert.AreEqual("forty", game.GetPlayer1ScoreName());
+
+        game.SetScores(4, 3);
+
+        Assert.AreEqual("forty", game.GetPlayer2ScoreName());
+        Assert.AreEqual("advantage", game.GetPlayer1ScoreName());
+    }
 }
