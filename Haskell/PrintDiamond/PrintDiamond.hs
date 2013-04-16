620d7bbb497d33da415c89134cfe01f2973ab295
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-16 16:42:54 -0400

    Finished PrintDiamond in Haskell

--- Haskell/PrintDiamond/PrintDiamond.hs
+++ Haskell/PrintDiamond/PrintDiamond.hs
@@ -10,3 +10,14 @@ distance x y = abs ((ord $ toUpper x) - (ord $ toUpper y))
 getPadding :: Int -> String
 getPadding 0 = ""
 getPadding n = [' ' | x <- [1..n]]
+
+getDiamondLine :: Char -> Char -> String
+getDiamondLine 'A' end = getPadding (distance 'A' end) ++ ['A']
+getDiamondLine cur end =  startPad ++ [cur] ++ endPad ++ [cur]
+ where startPad = getPadding (distance cur end)
+       endPad = getPadding (((distance 'A' cur)-1)*2 + 1)
+
+getDiamondLines :: Char -> [String]
+getDiamondLines end = [getDiamondLine c end | c <- absRange]
+ where range = ['A'..end]
+       absRange = range ++ (tail (reverse range))
--- Haskell/PrintDiamond/Test_PrintDiamond.hs
+++ Haskell/PrintDiamond/Test_PrintDiamond.hs
@@ -1,5 +1,4 @@
 module Test_PrintDiamond where
-
 import PrintDiamond
 import Test.HUnit
 
@@ -18,10 +17,28 @@ testGetPadding0ReturnsAnEmptyString =
 testGetPadding4ReturnsFourSpaces =
   TestCase (assertEqual "pad 4 -> '    '" "    " (getPadding 4))
 
+testGetDiamondLineFromAToEAtAIsFourSpacesAndA =
+  TestCase (assertEqual "getline A E -> '    A'" "    A" (getDiamondLine 'A' 'E'))
+
+testGetDiamondLineFromBToEWorks =
+  TestCase (assertEqual "getline B E works" "   B B" (getDiamondLine 'B' 'E'))
+
+testGetDiamondLineFromCToEWorks =
+  TestCase (assertEqual "getline C E works" "  C   C" (getDiamondLine 'C' 'E'))
+
+expected_diamond = ["  A", " B B", "C   C", " B B", "  A"]
+
+testGetDiamondLinesWorksFromAToC =
+  TestCase (assertEqual "getlines from a to c works" expected_diamond (getDiamondLines 'C'))
+
 tests = TestList [testDistanceFromAToAIs0,
                   testDistanceFromAToCIs2,
                   testDistanceFromLowerAToUpperAIs0,
                   testGetPadding0ReturnsAnEmptyString,
-                  testGetPadding4ReturnsFourSpaces]
+                  testGetPadding4ReturnsFourSpaces,
+                  testGetDiamondLineFromAToEAtAIsFourSpacesAndA,
+                  testGetDiamondLineFromBToEWorks,
+                  testGetDiamondLineFromCToEWorks,
+                  testGetDiamondLinesWorksFromAToC]
 
 main = runTestTT tests
