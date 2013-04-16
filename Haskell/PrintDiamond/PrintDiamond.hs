1993f15ce16faf059d1f870b28cdb8277b70d9d8
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-16 12:12:55 -0400

    Did more work on print diamond in Haskell

--- Haskell/PrintDiamond/PrintDiamond.hs
+++ Haskell/PrintDiamond/PrintDiamond.hs
@@ -1,7 +1,12 @@
 module PrintDiamond where
 
-distance :: Char -> Char -> Int
+import Data.Char
 
+distance :: Char -> Char -> Int
 distance 'A' y = abs (65 - (ord $ toUpper y))
 distance 'a' y = distance 'A' y
 distance x y = abs ((ord $ toUpper x) - (ord $ toUpper y))
+
+getPadding :: Int -> String
+getPadding 0 = ""
+getPadding n = [' ' | x <- [1..n]]
--- Haskell/PrintDiamond/Test_PrintDiamond.hs
+++ Haskell/PrintDiamond/Test_PrintDiamond.hs
@@ -9,7 +9,19 @@ testDistanceFromAToAIs0 =
 testDistanceFromAToCIs2 =
   TestCase (assertEqual "d(A, C) = 2" 2 (distance 'A' 'C'))
 
+testDistanceFromLowerAToUpperAIs0 =
+  TestCase (assertEqual "d(a, A) = 0 (case-insensitive)" 0 (distance 'a' 'A'))
+
+testGetPadding0ReturnsAnEmptyString =
+  TestCase (assertEqual "pad 0 -> ''" "" (getPadding 0))
+
+testGetPadding4ReturnsFourSpaces =
+  TestCase (assertEqual "pad 4 -> '    '" "    " (getPadding 4))
+
 tests = TestList [testDistanceFromAToAIs0,
-                  testDistanceFromAToCIs2]
+                  testDistanceFromAToCIs2,
+                  testDistanceFromLowerAToUpperAIs0,
+                  testGetPadding0ReturnsAnEmptyString,
+                  testGetPadding4ReturnsFourSpaces]
 
 main = runTestTT tests
