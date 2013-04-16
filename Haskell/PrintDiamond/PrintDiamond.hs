5ecc15efa6ea4570be699615307d70784ade0a0b
Michael J. Gilliland <mjg.py3@gmail.com>
2013-04-16 18:53:44 -0400

    Improved via hlint

--- Haskell/PrintDiamond/PrintDiamond.hs
+++ Haskell/PrintDiamond/PrintDiamond.hs
@@ -12,12 +12,12 @@ getPadding 0 = ""
 getPadding n = [' ' | x <- [1..n]]
 
 getDiamondLine :: Char -> Char -> String
-getDiamondLine 'A' end = getPadding (distance 'A' end) ++ ['A']
+getDiamondLine 'A' end = getPadding (distance 'A' end) ++ "A"
 getDiamondLine cur end =  startPad ++ [cur] ++ endPad ++ [cur]
  where startPad = getPadding (distance cur end)
-       endPad = getPadding (((distance 'A' cur)-1)*2 + 1)
+       endPad = getPadding (( distance 'A' cur -1)*2 + 1)
 
 getDiamondLines :: Char -> [String]
 getDiamondLines end = [getDiamondLine c end | c <- absRange]
  where range = ['A'..end]
-       absRange = range ++ (tail (reverse range))
+       absRange = range ++ tail (reverse range)
